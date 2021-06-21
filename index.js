41c51fb225cd8ee65120d13bf913cdb3e690a82b
lea.arturi <lea.arturi@gmail.com>
2021-06-21 12:54:28 -0300

    Ajuste menor

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const server = new ApolloServer({
     typeDefs,
     resolvers,
     context: ({req}) => {
+        
         const token = req.headers['authorization'] || '';
         if (token) {
             try {
